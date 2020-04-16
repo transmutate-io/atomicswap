bab71d2b54d74e37f50ab216f883c6722bbc8545
transmutateio <transmutate.io@gmail.com>
2020-04-16 20:24:40 +0100

    added methods to Key

--- types/key/key.go
+++ types/key/key.go
@@ -6,19 +6,24 @@ type (
 
 	Keyer interface{ Key() interface{} }
 
-	Private interface {
+	YAMLMarshalerUnmarshaler interface {
 		MarshalYAML() (interface{}, error)
 		UnmarshalYAML(unmarshal func(interface{}) error) error
+	}
+
+	Private interface {
 		Public() Public
 		Sign(b []byte) ([]byte, error)
+		YAMLMarshalerUnmarshaler
+		Serialize() []byte
 		Keyer
 	}
 
 	Public interface {
-		MarshalYAML() (interface{}, error)
-		UnmarshalYAML(unmarshal func(interface{}) error) error
 		Hash160() []byte
 		Verify(sig, msg []byte) error
+		YAMLMarshalerUnmarshaler
+		SerializeCompressed() []byte
 		Keyer
 	}
 )
